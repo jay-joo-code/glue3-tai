b45cf65011c710281b5c2efb0130ce72ca13d79b
Jay Joo <jj534@cornell.edu>
2023-02-28 14:58:52 -0500

    init supabase migration

--- src/lib/glue/pocketbase.ts
+++ src/lib/glue/pocketbase.ts
@@ -1,12 +1,1 @@
-import PocketBase from 'pocketbase';
-
-import { writable } from 'svelte/store';
-
-export const pb = new PocketBase('pocketbase url');
-export const currentUser = writable(pb.authStore.model);
-
-pb.autoCancellation(false);
-
-pb.authStore.onChange((auth) => {
-	currentUser.set(pb.authStore.model);
-});
+export const pb = null;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import AppLayout from '$lib/components/glue/AppLayout.svelte';
+	import { supabase } from '$lib/glue/supabaseClient';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
 <AppLayout>
