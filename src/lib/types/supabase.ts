be17109bb1f2559835cd79ff2be94ba4bf761336
Jay Joo <jj534@cornell.edu>
2023-02-28 15:29:03 -0500

    install supabase js sdk, remove email api route

--- src/lib/types/supabase.ts
+++ src/lib/types/supabase.ts
@@ -52,32 +52,6 @@ export interface Database {
           user_id?: string
         }
       }
-      profiles: {
-        Row: {
-          avatar_url: string | null
-          full_name: string | null
-          id: string
-          updated_at: string | null
-          username: string | null
-          website: string | null
-        }
-        Insert: {
-          avatar_url?: string | null
-          full_name?: string | null
-          id: string
-          updated_at?: string | null
-          username?: string | null
-          website?: string | null
-        }
-        Update: {
-          avatar_url?: string | null
-          full_name?: string | null
-          id?: string
-          updated_at?: string | null
-          username?: string | null
-          website?: string | null
-        }
-      }
       role_permissions: {
         Row: {
           id: number
